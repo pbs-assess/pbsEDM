96fe28078d7127a84de4610c0767781cbee57eb7
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-05-26 16:46:53 -0700

    Removed

--- tests/testthat/test-pbs-edm.R
+++
@@ -1,9 +0,0 @@
-context("test-pbs-edm.R")
-
-test_that("pbs_edm() returns a list with the correct length", {
-  data_frame <- data.frame(x = simple_ts)
-  lags <- list(x = 0:1)
-  edm_list <- pbs_edm(data_frame, lags, show_calculations = TRUE)
-  expect_true(is.list(edm_list))
-  expect_equal(length(edm_list), 12)
-})
