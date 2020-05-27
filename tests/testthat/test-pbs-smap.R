c30c390c1cd3d283dd1219385cf50b4c99c34cab
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-05-27 14:53:41 -0700

    Removed

--- tests/testthat/test-pbs-smap.R
+++
@@ -1,9 +0,0 @@
-context("test-pbs-smap.R")
-
-test_that("pbs_smap() returns a tibble with the correct dimensions", {
-	data_frame <- data.frame(x = simple_ts)
-	lags <- list(x = 0:1)
-	smap_list <- pbs_smap(data_frame, lags, show_calculations = TRUE)
-	expect_true(is.list(smap_list))
-	expect_equal(length(smap_list), 12)
-})
