192fbc94a3a7a537d14cff6a75042574da1b2b83
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-05-27 14:53:33 -0700

    Added test that pbsSMAP() returns a list

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -6,3 +6,10 @@ test_that("pbsEDM() returns a list", {
 	out_list <- pbsEDM(nt, lags)
 	expect_true(is.list(out_list))
 })
+
+test_that("pbsSMAP() returns a list", {
+	nt <- data.frame(x = simple_ts)
+	lags <- list(x = 0:1)
+	out_list <- pbsSMAP(nt, lags)
+	expect_true(is.list(out_list))
+})
