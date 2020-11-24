8889cbf2ecab5e9c7ad3aa9ec65468def1dc0ed7
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-11-24 15:24:23 -0800

    implemented pbsSimplex() for sequence of embedding dimensions E

--- R/methods.R
+++ R/methods.R
@@ -338,7 +338,7 @@ pbsEDM_Evec <- function(N_t,
 #' m3 <- pbsSmap(N, lags, first_difference = TRUE)
 #' 
 pbsSmap <- function (N,
-                     lags,
+                     lags = NULL,
                      theta = 0,
                      p = 1L,
                      first_difference = FALSE,
@@ -551,41 +551,76 @@ pbsSmap <- function (N,
   )
 }
 
-
-
+#' Forecast via Simplex Projection
+#'
+#' @param N [data.frame()] or [numeric()] Response variable time series as
+#'   first column or as a vector.
+#' @param E [integer()] Vector of embedding dimensions.
+#' @param p [integer()] Scalar forecast distance.
+#' @param first_difference [logical()] First-difference the time series
+#' @param centre_and_scale [logical()] Centre and scale the time series?
+#' @param verbose [logical()] Print progress?
+#'
+#' @return A list of class \code{pbsSimplex}
+#' @author Luke A. Rogers
+#' @export
+#'
+#' @examples
+#' N <- data.frame(x = 1:30)
+#' s1 <- pbsSimplex(N)
+#' 
+#' N <- data.frame(x = simple_ts)
+#' s2 <- pbsSimplex(N)   
+#' 
 pbsSimplex <- function (N,
-                        E = 2L,
+                        E = 1:10,
                         p = 1L,
                         first_difference = FALSE,
                         centre_and_scale = FALSE,
                         verbose = FALSE) {
   
-  #----------------- Check arguments ------------------------------------------#
+  # Check arguments ------------------------------------------------------------
   
   stopifnot(
-    is.vector(N),
-    is.numeric(N),
-    is.numeric(E) && length(E) == 1L && floor(E) == E && E > 0L,
+    is.data.frame(N) | (is.vector(N) & is.numeric(N)),
+    is.numeric(E) & floor(E) == E & E > 0L,
     is.integer(p) && length(p) == 1L,
     is.logical(first_difference) && length(first_difference) == 1L,
     is.logical(centre_and_scale) && length(centre_and_scale) == 1L,
     is.logical(verbose) && length(verbose) == 1L
   )
   
-  #----------------- Define N -------------------------------------------------#
+  # Define N -------------------------------------------------------------------
   
-  N <- data.frame(Obs = N)
+  if (is.numeric(N)) {
+    N <- data.frame(Obs = N)
+  } else {
+    colnames(N)[1] <- "Obs"
+  }
   
-  #----------------- Define lags ----------------------------------------------#
+  # Compute --------------------------------------------------------------------
   
-  lags <- list(Obs = seq(0L, E - 1))
+  results_list <- list()
+  results <- data.frame()
   
-  #----------------- Return value ---------------------------------------------#
+  # TODO: Parallelize this using sockets for compatibility with Windows
+  for (i in seq_along(E)) {
+    # Define lags
+    lags <- list(Obs = seq(0L, E[i] - 1))
+    # Store value
+    results_list[[i]] <- pbsEDM(N, 
+                                lags, 
+                                p, 
+                                first_difference, 
+                                centre_and_scale, 
+                                verbose = FALSE)
+    results <- rbind(results, results_list[[i]]$results)
+  }
+
+  # Return value ---------------------------------------------------------------
   
-  return(
-    structure(
-      pbsEDM(N, lags, p, first_difference, centre_and_scale, verbose),
-      class = c("pbsEDM", "pbsSimplex")
-    )
-  )
+  return(structure(list(
+    results_list = results_list,
+    results = results),
+    class = c("pbsSimplex")))
 }
