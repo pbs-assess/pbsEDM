a0d68926ede4459d9f88b1f086d9a0bd2c525493
luke-a-rogers <luke.allister.rogers@gmail.com>
2020-11-24 15:32:44 -0800

    updated utilities tests

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -1,47 +1,19 @@
 context("test-utilities.R")
-# pbsN()
-test_that("pbsN() errors when N is a <0 x 0 matrix>", {
-	
-})
-test_that("pbsN() errors when N is a data frame with 0 columns and 0 rows", {
-	
-})
-test_that("pbsN() errors when the columns of N are not numeric", {
-	
-})
-test_that("pbsN() errors when the elements of lags are not numeric", {
-	
-})
-test_that("pbsN() errors when the elements of lags are not vectors", {
-	
-})
-test_that("pbsN() errors when names(lags) are not in colnames(N)", {
-	
-})
-test_that("pbsN() errors when there are duplicates in names(lags)", {
-	
-})
-test_that("pbsN() errors when there are duplicates in colnames(N)", {
-	
-})
-test_that("pbsN() errors when lags[[1]][1] is not equal to 0L", {
-	
-})
-test_that("pbsN() errors when p is not a scalar integer", {
-	
-})
-# pbsZ()
 
+# Test pbsLag ------------------------------------------------------------------
 
-test_that("pbsLag() returns a vector or matrix depending on the input", {
-	x1 <- c(1:10)
-	x2 <- matrix(rep(1:10, 2), ncol = 2)
-	m1 <- pbsLag(x1, 1)
-	m2 <- pbsLag(x2, c(1, 2))
-	expect_true(is.numeric(m1))
-	expect_true(is.matrix(m2))
-})	
+test_that("pbsLag() returns a vector for vector input", {
+	x <- 0
+	y <- pbsLag(x)
+	expect_vector(y)
+})
 
+test_that("pbsLag() returns a matrix for matrix input", {
+	x <- matrix(0, nrow = 1, ncol = 1)
+	y <- pbsLag(x)
+	expect_true(is.matrix(y))
+})
+# TODO: Update from here
 test_that("pbsLag() returns the correct valued vector or matrix", {
 	x1 <- c(1:10)
 	x2 <- matrix(rep(1:10, 2), ncol = 2)
@@ -65,4 +37,8 @@ test_that("pbsLag() recycles input lags of length one only", {
 	expect_equal(m32, t32)
 	expect_equal(m33, t33)
 })
+
+
+
+
 
